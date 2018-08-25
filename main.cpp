ce32d3e3119686c994fc6c37af1ccc0864959d8b
liu <164346864>
2018-08-25 16:38:47 +0800

    '自动创建日志文件夹'

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
@@ -27,44 +27,59 @@ namespace logging = boost::log;
 namespace expr = boost::log::expressions;
 namespace ps = boost::process;
 
-static HWND hmessage{};
+static HWND hmessage {};
 static std::size_t i = 0;
-static NOTIFYICONDATA notify{};
+static NOTIFYICONDATA notify {};
 static const int WM_TRAYICON = WM_USER + 1;
 static ch::steady_clock::time_point process_start;
 
-LRESULT CALLBACK tray(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
+struct config {
+	std::string log;
+	std::string runable;
+	std::vector<int> rtcode;
+};
+
+inline bool mkdirs(const char* dir, uint32_t length) {
+	if (!length) length = strlen(dir);
+	if (length > MAX_PATH) return false;
+	char strbuffer[MAX_PATH] = {};
+	for (uint32_t i = 0; i < length && (strbuffer[i] = dir[i]); ++i) {
+		if (strbuffer[i] == '\\' && access(strbuffer, F_OK) != 0) {
+			if (mkdir(strbuffer) != 0) return false;
+		}
+	}
+	return true;
+}
+
+LRESULT CALLBACK tray(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 		case WM_TRAYICON:
 			switch (lParam) {
-				case WM_LBUTTONUP:
-					{
-						auto process_duration = ch::duration_cast<ch::minutes>(ch::steady_clock::now() - process_start);
-						sprintf(notify.szInfo, "已经守护 %d 次, 当前存活 %d 分钟", i-1, process_duration.count());
-						strcpy(notify.szInfoTitle, "提示");
-						notify.uTimeout = 2000;
-						notify.uFlags = NIF_INFO;
-						notify.dwInfoFlags= NIIF_INFO;
-						notify.cbSize = sizeof(NOTIFYICONDATA);
-						Shell_NotifyIcon(NIM_MODIFY, &notify);
-						return TRUE;
-					}
-					break;
-				case WM_RBUTTONUP:
-					{
-						POINT pt;
-						GetCursorPos(&pt);
-						UINT uFlags = TPM_RIGHTBUTTON;
-						HMENU menu = CreatePopupMenu();
-						AppendMenu(menu, MF_STRING , 0, "退出守护");
-						uFlags |= GetSystemMetrics(SM_MENUDROPALIGNMENT)?TPM_RIGHTALIGN:TPM_LEFTALIGN;
-						SetForegroundWindow(hmessage);
-						TrackPopupMenuEx(menu, uFlags, pt.x, pt.y, hwnd, nullptr);
-						DestroyMenu(menu);
-						return TRUE;
-					}
-					break;
+				case WM_LBUTTONUP: {
+					auto process_duration = ch::duration_cast<ch::minutes>(ch::steady_clock::now() - process_start);
+					sprintf(notify.szInfo, "已经守护 %d 次, 当前存活 %d 分钟", i-1, process_duration.count());
+					strcpy(notify.szInfoTitle, "提示");
+					notify.uTimeout = 2000;
+					notify.uFlags = NIF_INFO;
+					notify.dwInfoFlags= NIIF_INFO;
+					notify.cbSize = sizeof(NOTIFYICONDATA);
+					Shell_NotifyIcon(NIM_MODIFY, &notify);
+					return TRUE;
+				}
+				break;
+				case WM_RBUTTONUP: {
+					POINT pt;
+					GetCursorPos(&pt);
+					UINT uFlags = TPM_RIGHTBUTTON;
+					HMENU menu = CreatePopupMenu();
+					AppendMenu(menu, MF_STRING , 0, "退出守护");
+					uFlags |= GetSystemMetrics(SM_MENUDROPALIGNMENT)?TPM_RIGHTALIGN:TPM_LEFTALIGN;
+					SetForegroundWindow(hmessage);
+					TrackPopupMenuEx(menu, uFlags, pt.x, pt.y, hwnd, nullptr);
+					DestroyMenu(menu);
+					return TRUE;
+				}
+				break;
 			}
 			break;
 		case WM_COMMAND:
@@ -73,36 +88,30 @@ LRESULT CALLBACK tray(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 				return TRUE;
 			}
 	}
-    return DefWindowProc(hwnd, message, wParam, lParam);
+	return DefWindowProc(hwnd, message, wParam, lParam);
 }
-
-struct config {
-	std::string log;
-	std::string runable;
-	std::vector<int> rtcode;
-};
 
 void protect(boost::shared_ptr<struct config> setting) {
 	auto ls = logging::add_file_log(
-        logging::keywords::file_name = setting->log,
-        logging::keywords::time_based_rotation = logging::sinks::file::rotation_at_time_point(0, 0, 0),
-        logging::keywords::format = (
-            expr::stream
-                << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "【%Y-%m-%d %H:%M:%S】")
-                << " [" << logging::trivial::severity
-                << "] " << expr::smessage
-        )
-    );
-    
-    if (!ls) MessageBox(nullptr, "创建日志失败！", "错误", MB_OK|MB_ICONERROR);
-    
+	              logging::keywords::file_name = setting->log,
+	              logging::keywords::time_based_rotation = logging::sinks::file::rotation_at_time_point(0, 0, 0),
+	              logging::keywords::format = (
+	                          expr::stream
+	                          << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "【%Y-%m-%d %H:%M:%S】")
+	                          << " [" << logging::trivial::severity
+	                          << "] " << expr::smessage
+	                      )
+	          );
+
+	if (!ls) MessageBox(nullptr, "创建日志失败！", "错误", MB_OK|MB_ICONERROR);
+
 	bool shoudQuit = false;
 	auto thread = boost::thread([&shoudQuit, setting] {
 		WNDCLASSEX wx{};
-	    wx.lpfnWndProc = tray;
-	    wx.lpszClassName = "super deamon";
-	    wx.cbSize = sizeof(WNDCLASSEX);
-	    RegisterClassEx(&wx);
+		wx.lpfnWndProc = tray;
+		wx.lpszClassName = "super deamon";
+		wx.cbSize = sizeof(WNDCLASSEX);
+		RegisterClassEx(&wx);
 		notify.hIcon = LoadIcon(GetModuleHandle(nullptr), "A");
 		notify.hWnd = hmessage = CreateWindowEx(0, wx.lpszClassName, "tray", 0, 0, 0, 0, 0, HWND_MESSAGE, nullptr, nullptr, nullptr);
 		notify.uFlags = NIF_ICON|NIF_MESSAGE|NIF_TIP;
@@ -124,10 +133,10 @@ void protect(boost::shared_ptr<struct config> setting) {
 		UnregisterClass(wx.lpszClassName, GetModuleHandle(nullptr));
 		shoudQuit = true;
 	});
-    
-    logging::add_common_attributes();
+
+	logging::add_common_attributes();
 	logging::sources::severity_logger<logging::trivial::severity_level> lg;
-	
+
 	std::error_code error_code;
 	auto rtcodes = setting->rtcode;
 	while (!shoudQuit) {
@@ -139,7 +148,7 @@ void protect(boost::shared_ptr<struct config> setting) {
 		auto process_duration = ch::duration_cast<ch::minutes>(ch::steady_clock::now() - process_start);
 		int rtcode = child.exit_code();
 		BOOST_LOG_SEV(lg, logging::trivial::info) << "进程已退出，代码："
-												<< rtcode << "，存活时间：" << process_duration.count() << "（分钟）" << std::endl;
+		        << rtcode << "，存活时间：" << process_duration.count() << "（分钟）" << std::endl;
 
 		if (ls) ls->flush();
 		if (std::find(rtcodes.begin(), rtcodes.end(), rtcode) != rtcodes.end()) break;
@@ -150,9 +159,8 @@ void protect(boost::shared_ptr<struct config> setting) {
 	}
 }
 
-int main(int argc, char* argv[])
-{
-	char buffer[MAX_PATH]{};
+int main(int argc, char* argv[]) {
+	char buffer[MAX_PATH] {};
 	const char* self = argv[0];
 	const char* sep = strrchr(self, '\\');
 	if (sep != nullptr) {
@@ -208,9 +216,13 @@ int main(int argc, char* argv[])
 			if (setting->rtcode.empty()) setting->rtcode.push_back(0);
 			if (!setting->runable.empty()) {
 				if (access(setting->runable.data(), 0)) {
-					MessageBox(nullptr, "目标文件文件不存在！", "错误", MB_OK|MB_ICONERROR);
+					MessageBox(nullptr, "目标文件不存在！", "错误", MB_OK|MB_ICONERROR);
 				} else {
-					protect(setting);
+					bool yes = true;
+					if (!mkdirs(setting->log.data(), setting->log.length())) {
+						yes = MessageBox(nullptr, "无法创建日志文件夹，是否继续？", "错误", MB_YESNO|MB_ICONERROR) == IDYES;
+					}
+					if (yes) protect(setting);
 				}
 			} else {
 				MessageBox(nullptr, "未配置目标文件！", "错误", MB_OK|MB_ICONERROR);
